0ceffcba6568b3499cdedfbaeb4e1c176e55b391
venslavoviczena-rgb <venslavoviczena@gmail.com>
2026-04-15 08:27:31 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+// 1. ИНИЦИАЛИЗАЦИЯ ДАННЫХ
+// Пытаемся загрузить данные, если их нет используем пустой массив
+const rawData = localStorage.getItem("film_list");
+// console.log(rawData);
+
+let items = rawData ? JSON.parse(rawData) : [];
+
+// Ссылки на элементы DOM
+
+const container = document.querySelector("#list-container");
+console.log(container)
+// Функция отрисовки интерфейса
+function render() {
+  container.innerHTML = "";
+  // Очищаем список перед перерисовкой
+  items.forEach((film) => {
+    const li = document.createElement("li");
+    console.log(li)
+    //li.className = "item";
+    console.log(film.film_url);
+    const url = new URL(`${film.film_url}`);
+    console.log(url.pathname);
+    const src_path = `https://www.youtube.com/embed${url.pathname}`;
+
+    // Добавляем текст в элемент списка
+    li.innerHTML = `<div class="thumb-wrap">
+    <span> Название: ${film.title}</span>                   
+    <iframe
+      src= ${src_path}  
+      allowfullscreen
+    ></iframe>
+    </div>
+    
+    `;
+    container.appendChild(li);
+  });
+}
+// https:
+//youtu.be/To7NUGcuczI
+// Добавление нового товара
+// https:
+
+// 3. ЗАПУСК ПРИ СТАРТЕ
+// Вызываем рендер сразу, чтобы показать сохраненные данные
+render();
